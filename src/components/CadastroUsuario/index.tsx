0ac0660ebc64e8aa8c97b82074521dc2a2330bcd
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-09-06 00:40:02 -0300

    criando componentes

---
+++ src/components/CadastroUsuario/index.tsx
@@ -0,0 +1,70 @@
+import * as C from '../App.Styles'
+import logo from '../../images/logo.png'
+import './styles.css'
+
+export const CadastroUsuario = () => {
+    return(
+        <C.Wrapper>
+            <C.largerContainer>
+            <form className='Auth-form-container'>
+            <div className="Auth-form-img">  <img src={logo}></img></div>
+        <h3>CADASTRO</h3>
+        <div className="mb-3">
+          <label>Nome</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="nome"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Email</label>
+          <input type="email" className="form-control" placeholder="email" />
+        </div>
+        <div className="mb-3">
+          <label>Senha</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="senha"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Confirmar senha</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="confirmar senha"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Unidade/Apartamento</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="senha"
+          />
+          </div>
+           <div className="mb-3">
+          <label>Link da foto</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="senha"
+          />
+          </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/sign-in">sign in?</a>
+        </p>
+      </form>
+            </C.largerContainer>
+        </C.Wrapper>
+
+
+    )
+}
---
+++ src/components/LoginParrot/index.tsx
@@ -0,0 +1,48 @@
+import * as C from '../App.Styles'
+import "bootstrap/dist/css/bootstrap.min.css"
+import './styles.css'
+import logo from '../../images/logo.png'
+
+export const LoginParrot = () => {
+   return(
+        <C.Wrapper>
+          <C.Container>
+        
+           
+      <form className="Auth-form">
+      <div className="Auth-form-img">  <img src={logo}></img></div>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Login</h3>
+          <div className="form-group mt-3">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Senha</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="senha"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn-btn-primary">
+              Entrar
+            </button>
+          </div>
+          <p className="forgot-password text-right mt-2">
+             <a href="#">Cadastre-se</a>
+          </p>
+        </div>
+      </form>
+    
+
+          </C.Container>
+        </C.Wrapper>
+        
+   )
+}
