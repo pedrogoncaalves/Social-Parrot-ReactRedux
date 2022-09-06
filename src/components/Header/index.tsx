9a9c55f0e8688d1237c0c33dcfa67570975d56bb
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-09-06 19:13:52 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
-
-
+import navbarbrand from '../../images/navbarbrand.jpg';
+import './styles.css'
 import { Container, Navbar } from 'react-bootstrap';
 
 
 
 export const Header = () => {
     return(
-        <Navbar>
+        <Navbar fixed="top" className="navbar__container" >
         <Container>
-          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={navbarbrand}></img></Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              Olá,  <a href="#login">Mark Otto</a>
+            <Navbar.Text color="#76BB4C">
+              Olá,  <a href="#login">usuário</a>
+              <button className='btn-logout'>sair</button>
             </Navbar.Text>
           </Navbar.Collapse>
         </Container>
