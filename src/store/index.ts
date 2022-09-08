338946711e8b89b5777d783fb2c5b792a0eebd5a
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-09-08 11:40:22 -0300

    iniciando redux

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from  "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer: {},
+})
+
+
+export type RootStore = ReturnType<typeof store.getState>;
+
+
+export default store;
+
